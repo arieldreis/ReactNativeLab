37803985525e8fdd957117b064d82192ab5aead6
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-04-12 21:49:54 -0300

    Add initial project setup with package.json and style definitions
    - Created package.json for the React Native project with dependencies and scripts for Expo.
    - Added style.js to define styles for the application, including container and font styles.

---
+++ REACT_NATIVE_COURSE/rnCourse/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { styles } from './style.js';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.font}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ REACT_NATIVE_COURSE/rnCourse/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#fff000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  font: {
+    fontSize: 20,
+    color: "#fff",
+    textShadow: "1px 1px 1px #000"
+  }
+});
