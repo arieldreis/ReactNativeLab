3c6177347e454fcb7a4ec5d79d64164bc56b0d02
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-04-15 23:45:10 -0300

    Learning and use hookies

--- REACT_NATIVE_COURSE/rnCourse/App.js
+++ REACT_NATIVE_COURSE/rnCourse/App.js
@@ -1,15 +1,30 @@
 import { Text, View, Button, TextInput } from 'react-native';
 import { styles } from './style.js';
+import { useState } from 'react';
 
 const App = () => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText){
+    setEnteredGoalText(enteredText)
+  };
+  function addGoalHandler(){
+    console.log(enteredGoalText);
+  };
+
+
   return (
     <View style={styles.AppContainer}>
       <View style={styles.inputContainer}>
         <TextInput 
         placeholder='Type your goal here...' 
         style={styles.textInput}
+        onChange={goalInputHandler}
         />
-        <Button title="Tap Me!"/>
+        <Button 
+        title="Tap Me!"
+        onPress={addGoalHandler}
+        />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
